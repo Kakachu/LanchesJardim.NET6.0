fdd29102905c4ce5edbaaee5b9a4c3c62745328c
kakachu <kauajardim2004@hotmail.com>
2022-06-30 13:57:14 -0300

    Criando LacnheController e View para Lanches

---
+++ LanchesJardim.NET6.0/Controllers/LancheController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesJardim.NET6._0.Repositories.Interfaces;
+using LanchesJardim.NET6._0.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJardim.NET6._0.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
+        }
+    }
+}
---
+++ LanchesJardim.NET6.0/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesJardim.NET6._0.Models;
+
+namespace LanchesJardim.NET6._0.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
